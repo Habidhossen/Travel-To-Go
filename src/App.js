1ad12bc6786a61ab17ecc6b491ca9a0d63bbd906
Habidhossen <habidhossen2@gmail.com>
2022-04-18 19:59:08 +0600

    react router and react bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
