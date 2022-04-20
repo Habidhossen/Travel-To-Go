da4d0d595a1e91c3af84f3f4bedb2c4389950f6a
Habidhossen <habidhossen2@gmail.com>
2022-04-20 12:33:37 +0600

    about me section added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,20 @@
 import React from "react";
+import profileImg from "../../assets/images/profile-img.jpg";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      <h5>Habid Hossen Hridoy</h5>
+    <div className="container mt-5 bg-light p-5 border-5">
+      <div className="d-flex justify-content-center mb-3">
+        <img className="about-img" src={profileImg} alt="" />
+      </div>
+      <h3 className="about-title">Habid Hossen Hridoy</h3>
+      <p className="about-desc">
+        Hello! I'm Habid Hossen Hridoy. I'm from Chattogram, Bangladesh. I am a
+        student of Computer Science and Engineering. I like to Code, Design,
+        Innovate and Experiment. I want to become a Full Stack Developer within
+        the next 3-4 months.
+      </p>
     </div>
   );
 };
